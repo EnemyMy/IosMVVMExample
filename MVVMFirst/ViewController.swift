6a290317e4265352254bf8f353251cffcbb524fb
EnemyMy <alexei.blizzard@gmail.com>
2021-01-01 15:01:49 +0200

    Initial Commit

---
+++ MVVMFirst/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMFirst
+//
+//  Created by Алексей on 01.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
